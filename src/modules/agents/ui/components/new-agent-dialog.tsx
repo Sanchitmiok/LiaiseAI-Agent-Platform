215ce0f100537924dc986cd3f33020afe90604ed
Sanchit Mishra <sanchitmishra9795@gmail.com>
2025-06-26 10:23:49 +0530

    Add new agent and protect routes

---
+++ src/modules/agents/ui/components/new-agent-dialog.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { ResponsiveDialog } from "@/components/responsive-dialog"
+import { AgentForm } from "./agent-form";
+
+interface NewAgentDialogProps {
+    open : boolean;
+    openChange : (open: boolean) => void;
+}
+
+export const NewAgentDialog = ({
+    open , openChange
+} : NewAgentDialogProps) => {
+
+    return (
+        <ResponsiveDialog
+            open={open}
+            onOpenChange={openChange}
+            title="New Agent"
+            description="Create a new agent."
+        >
+        <AgentForm onSuccess={() => openChange(false)} onCancel={() => openChange(false)}/>
+        </ResponsiveDialog>
+    )
+}
