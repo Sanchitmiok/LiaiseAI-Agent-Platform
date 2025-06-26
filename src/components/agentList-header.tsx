a7a47c1f04d978b7854410b7bc35b74c5381ca97
Sanchit Mishra <sanchitmishra9795@gmail.com>
2025-06-26 12:47:35 +0530

    Add agents data table

--- src/components/agentList-header.tsx
+++ src/components/agentList-header.tsx
@@ -7,9 +7,9 @@ import { NewAgentDialog } from "@/modules/agents/ui/components/new-agent-dialog"
 function AgentListHeader() {
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
   return (
-    <div>
+    <div className="pb-4">
       <NewAgentDialog open={isDialogOpen} openChange={setIsDialogOpen} />
-      <div className="py-4 px-4 md:px-8 bg-white border-b flex flex-col gap-y-4">
+      <div className="py-4 px-4 md:px-8 bg-accent border-b flex flex-col gap-y-4 ">
         <div className="flex items-center justify-between">
           <h5 className="font-semibold md:text-xl">My Agents</h5>
           <Button onClick={() => setIsDialogOpen(true)}>
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -6,20 +6,22 @@ import {
 } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  //TODO: Change 'getMany' to  use 'ProtectedProcedure' when authentication is implemented
   getMany: protectedProcedure.query(async () => {
     const data = await db.select().from(agents);
     return data;
   }),
-  //TODO: Change 'getOne' to  use 'ProtectedProcedure' when authentication is implemented
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input }) => {
       const [existingAgent] = await db
-        .select()
+        .select({
+          //TODO: Change to actual count
+          meetingCount : sql<number>`5`,
+          ...getTableColumns(agents)
+        })
         .from(agents)
         .where(eq(agents.id, input.id));
       return existingAgent;
