e26efb2696d6112fce5451025b8770ad5a7f896a
Sanchit Mishra <sanchitmishra9795@gmail.com>
2025-07-01 18:50:34 +0530

    Implement authentication checks and enhance meeting management UI components

--- src/app/(dashboard)/agents/[agentId]/page.tsx
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from "@/lib/auth";
 import { AgentIdView, AgentIdviewError, AgentIdviewLoading } from "@/modules/agents/ui/views/agentId-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -9,6 +12,13 @@ interface Props {
 }
 
 const Page = async ({ params }: Props) => {
+  const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+  
+    if (!session) {
+      redirect("/sign-in");
+    }
   const { agentId } = await params;
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
---
+++ src/modules/meetings/ui/components/meeting-id-view-header.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { ChevronRightIcon, MoreVerticalIcon, PencilIcon, TrashIcon } from "lucide-react";
+import Link from "next/link";
+
+// Components
+// This component is used to display the header for the agent ID view page, including breadcrumbs for
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+// Dropdown menu for actions like edit and remove
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+//Button
+import { Button } from "@/components/ui/button";
+
+
+interface Props {
+  meetingId: string;
+  meetingName: string;
+  onEdit: () => void;
+  onRemove: () => void;
+}
+
+export const MeetingIdViewHeader = ({
+  meetingId,
+  meetingName,
+  onEdit,
+  onRemove,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl">
+              <Link href="/agents">My meetings</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="text-foreground text-xl font-medium [&svg]:size-4">
+            <ChevronRightIcon />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl text-black">
+              <Link href={`/agents/${meetingId}`}>{meetingName}</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+
+{/* Without modal={false}, the dialog that this dropdown opens cause the website to get stuck */}
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={onEdit}>
+            <PencilIcon className="mr-2 size-4" />
+            Edit</DropdownMenuItem>
+          <DropdownMenuItem onClick={onRemove}>
+            <TrashIcon className="mr-2 size-4" />
+            Remove</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
